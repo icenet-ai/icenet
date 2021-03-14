210f4f2b62981a6d5e17c3df4863f8f3acd7dda1
Jim Circadian <jamescircadian@gmail.com>
2021-03-14 22:07:14 +0000

    Had a play with doing this via SLURM, needs tuning/consideration

--- analyse.py
+++ analyse.py
@@ -22,6 +22,8 @@ import icenet2.config as config
 import icenet2.utils as utils
 
 from distributed import Client
+#from dask_jobqueue import SLURMCluster
+
 
 
 ####################################################################
@@ -36,6 +38,14 @@ if __name__ == '__main__':
     args = parse_args()
 
     dask.config.set(temporary_directory=os.path.expandvars("/tmp/$USER/dask-tmp"))
+
+    # TODO: This will run, but don't do it, it needs more testing
+#    cluster = SLURMCluster(cores=64,
+#                           memory="32 GB",
+#                           project="short",
+#                           walltime="00:30:00",
+#                           queue="short")
+#    cluster.scale(jobs=args.time)
     client = Client(n_workers=8)
 
     ####################################################################
@@ -95,14 +105,6 @@ if __name__ == '__main__':
 
     #    dask.visualize(forecast_da, format='svg', filename="test")
 
-    # <xarray.DataArray 'stack-3c744aedb18a4507d557016ee243bcc2' (time: 2192, yc: 432, xc: 432, leadtime: 186)>
-    # dask.array<getitem, shape=(2192, 432, 432, 186), dtype=float32, chunksize=(1, 432, 432, 1), chunktype=numpy.ndarray>
-    # Coordinates:
-    #   * time      (time) datetime64[ns] 2012-01-01 2012-01-02 ... 2017-12-31
-    #   * yc        (yc) float64 5.388e+03 5.362e+03 ... -5.362e+03 -5.388e+03
-    #   * xc        (xc) float64 -5.388e+03 -5.362e+03 ... 5.362e+03 5.388e+03
-    #   * leadtime  (leadtime) int64 1 2 3 4 5 6 7 8 ... 180 181 182 183 184 185 186
-
     print("Forecast DS")
     forecast_ds.info()
 
@@ -158,31 +160,6 @@ if __name__ == '__main__':
     progress(mae)
     mae.compute()
 
-    # >>> mae_da # TEST
-    # <xarray.DataArray (time: 9, leadtime: 186)>
-    # dask.array<mul, shape=(9, 186), dtype=float64, chunksize=(1, 1), chunktype=numpy.ndarray>
-    # Coordinates:
-    #   * time      (time) datetime64[ns] 2012-01-01 2012-01-02 ... 2012-01-09
-    #   * leadtime  (leadtime) int64 1 2 3 4 5 6 7 8 ... 180 181 182 183 184 185 186
-    # >>> mae_da.compute()
-    # <xarray.DataArray (time: 9, leadtime: 186)>
-    # array([[4.47474815, 5.0133635 , 4.9317736 , ..., 6.11015493, 5.96323838,
-    #         6.07493676],
-    #        [4.57382763, 5.0326483 , 4.93432715, ..., 6.35651186, 6.33104949,
-    #         6.2287885 ],
-    #        [4.84749644, 5.184488  , 5.16307038, ..., 6.5933675 , 6.49747025,
-    #         6.52474704],
-    #        ...,
-    #        [4.62127685, 5.25285173, 5.09580409, ..., 6.06727758, 5.90394006,
-    #         5.95896276],
-    #        [4.58940307, 4.9673342 , 4.99307693, ..., 5.87748405, 5.85600548,
-    #         5.86364249],
-    #        [4.85256254, 5.06698623, 4.96427311, ..., 5.99312671, 5.82945896,
-    #         5.94444338]])
-    # Coordinates:
-    #   * time      (time) datetime64[ns] 2012-01-01 2012-01-02 ... 2012-01-09
-    #   * leadtime  (leadtime) int64 1 2 3 4 5 6 7 8 ... 180 181 182 183 184 185 186
-
     dur = time() - tic
     print("Done in {}m:{:.0f}s.\n\n ".format(np.floor(dur / 60), dur % 60))
 
@@ -191,16 +168,4 @@ if __name__ == '__main__':
     mae_df.to_csv('temp.csv')
 
     client.close()
-
-
-
-
-
-
-
-
-
-
-
-
-
+    #cluster.close()
