218e7c0e16c7117c7dc428a9fe68434d2eeb5199
James Byrne <jambyr@bas.ac.uk>
2021-11-09 15:40:56 +0000

    Prediction bugfix in zip call

--- icenet2/model/predict.py
+++ icenet2/model/predict.py
@@ -164,8 +164,8 @@ def main():
         logging.info("Saving outputs generated for these inputs as well...")
         gen_dir = os.path.join(output_dir, "loader")
 
-        outputs = ((dates, gen_outputs, "outputs"),
-                   (dates, sample_weights, "weights"))
+        outputs = ((dates, gen_outputs, ["outputs"]),
+                   (dates, sample_weights, ["weights"]))
 
         for output_type in outputs:
             for date, output, directory in zip(*output_type):
