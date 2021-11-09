5ffc8a5a4ab0c55f2c8ce71bf0680d9f019adf3e
James Byrne <jambyr@bas.ac.uk>
2021-11-09 17:35:13 +0000

    Give me strength, not operating on full tilt today: bugfix

--- icenet2/model/predict.py
+++ icenet2/model/predict.py
@@ -169,15 +169,14 @@ def main():
         outputs = ((dates, gen_outputs, ["outputs"]),
                    (dates, sample_weights, ["weights"]))
 
-        for output_type in outputs:
-            for dates, outputs, directory in output_type:
-                for date, output in zip(dates, outputs):
-                    output_directory = os.path.join(gen_dir, directory)
-                    os.makedirs(output_directory, exist_ok=True)
-                    output_path = os.path.join(output_directory,
-                                               date.strftime("%Y_%m_%d.npy"))
+        for dates, outputs, directory in outputs:
+            for date, output in zip(dates, outputs):
+                output_directory = os.path.join(gen_dir, directory)
+                os.makedirs(output_directory, exist_ok=True)
+                output_path = os.path.join(output_directory,
+                                           date.strftime("%Y_%m_%d.npy"))
 
-                    logging.info("Saving {} - generated {} {}".
-                                 format(date, directory, output.shape))
-                    np.save(output_path, output)
+                logging.info("Saving {} - generated {} {}".
+                             format(date, directory, output.shape))
+                np.save(output_path, output)
 
