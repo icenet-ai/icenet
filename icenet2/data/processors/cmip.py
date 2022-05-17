bd7969bf9f105fe3a1da1375544f8b96d13a2950
James Byrne <jambyr@bas.ac.uk>
2022-05-17 14:03:25 +0100

    Developing #16: sorting out SIC processing for CMIP

--- icenet2/data/processors/cmip.py
+++ icenet2/data/processors/cmip.py
@@ -1,3 +1,5 @@
+import logging
+
 from icenet2.data.cli import process_args, process_date_args
 from icenet2.data.process import IceNetPreProcessor
 from icenet2.data.sic.mask import Masks
@@ -5,12 +7,16 @@ from icenet2.data.processors.utils import SICInterpolation
 
 
 class IceNetCMIPPreProcessor(IceNetPreProcessor):
-    def __init__(self, source, member, *args, **kwargs):
+    def __init__(self,
+                 source, member, *args,
+                 update_key=None, **kwargs):
         cmip_source = "{}.{}".format(source, member)
         super().__init__(*args,
                          identifier="cmip.{}".format(cmip_source),
                          source_suffix=cmip_source,
-                         update_key=cmip_source,
+                         update_key=cmip_source
+                         if not update_key
+                         else "{}.{}".format(update_key, cmip_source),
                          **kwargs)
 
     def pre_normalisation(self, var_name, da):
@@ -30,7 +36,7 @@ def main():
     )
     dates = process_date_args(args)
 
-    pp = IceNetCMIPPreProcessor(
+    cmip = IceNetCMIPPreProcessor(
         args.source,
         args.member,
         ["uas", "vas"],
@@ -46,7 +52,31 @@ def main():
         south=args.hemisphere == "south",
     )
     # ./data/cmip6/north/vas/MRI-ESM2-0.r2i1p1f1/2050/latlon_2050_01_22.nc
-    pp.init_source_data(
+    cmip.init_source_data(
         lag_days=args.lag,
     )
-    pp.process()
+    cmip.process()
+
+    logging.info("SIC PROCESSING")
+
+    cmip_sic = IceNetCMIPPreProcessor(
+        args.source,
+        args.member,
+        ["siconca"],
+        [],
+        args.name,
+        dates["train"],
+        dates["val"],
+        dates["test"],
+        linear_trends=["siconca"],
+        linear_trend_days=args.forecast_days,
+        north=args.hemisphere == "north",
+        ref_procdir=args.ref,
+        south=args.hemisphere == "south",
+        update_key="siconca",
+    )
+    cmip_sic.init_source_data(
+        lag_days=args.lag,
+        lead_days=args.forecast_days,
+    )
+    cmip_sic.process()
