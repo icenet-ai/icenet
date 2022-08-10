4f94334f1c49d6c4f5f87cba1f13e15d0c81115d
James Byrne <jambyr@bas.ac.uk>
2022-08-10 23:35:22 +0100

    Dev #55: implemented video for comparing with obs SIC

--- icenet2/plotting/forecast.py
+++ icenet2/plotting/forecast.py
@@ -1,8 +1,10 @@
 import argparse
 import logging
+import os
 
 import matplotlib
 import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
 
 from icenet2.data.cli import date_arg
 from icenet2.plotting.utils import get_forecast_obs_ds
@@ -13,14 +15,97 @@ matplotlib.rcParams.update({
 })
 
 
-def plot_sic_error(fc_da, obs_da, land_mask):
+def plot_sic_error(fc_da: object,
+                   obs_da: object,
+                   land_mask: object,
+                   output_path: object =
+                   os.path.join("plot", "sic_error.mp4")) -> object:
     """
 
     :param fc_da:
     :param obs_da:
     :param land_mask:
+    :param output_path:
     """
-    raise NotImplementedError(fc_da, obs_da, land_mask)
+
+    diff = fc_da - obs_da
+
+    def update(date):
+        logging.debug("Plotting {}".format(date.strftime("%D")))
+
+        maps[0].set_title("IceNet {}".format(
+            fc_da.isel(time=date).time.strftime("%d/%m/%Y")))
+        maps[1].set_title("OSISAF Obs {}".format(
+            obs_da.isel(time=date).time.strftime("%d/%m/%Y")))
+
+        maps[0].contourf(fc_plot, **contour_kwargs)
+        maps[1].contourf(obs_plot, **contour_kwargs)
+        maps[2].contourf(diff_plot, **diff_kwargs,
+                         vmin=-1, vmax=1, cmap="RdBu_r")
+
+        return maps
+
+    fig, maps = plt.subplots(nrows=1, ncols=3, figsize=(18, 5))
+    fig.set_dpi(150)
+
+    leadtime = 0
+    fc_plot = fc_da.isel(time=leadtime).to_numpy()
+    obs_plot = obs_da.isel(time=leadtime).to_numpy()
+    diff_plot = diff.isel(time=leadtime).to_numpy()
+
+    contour_kwargs = dict(
+        levels=200,
+        vmin=0,
+        vmax=1,
+        cmap='YlOrRd'
+    )
+    diff_kwargs = {k: v for k, v in contour_kwargs.items()
+                   if k not in ["vmin", "vmax", "cmap"]}
+
+    ctf = maps[0].contourf(fc_plot, **contour_kwargs)
+    cto = maps[1].contourf(obs_plot, **contour_kwargs)
+    ctd = maps[2].contourf(diff_plot, **diff_kwargs,
+                           vmin=-1, vmax=1, cmap="RdBu_r")
+
+    maps[0].set_title("IceNet {}".format(
+        fc_da.isel(time=leadtime).time.strftime("%d/%m/%Y")))
+    maps[1].set_title("OSISAF Obs {}".format(
+        obs_da.isel(time=leadtime).time.strftime("%d/%m/%Y")))
+    maps[2].set_title("Diff")
+
+    p0 = maps[0].get_position().get_points().flatten()
+    p1 = maps[1].get_position().get_points().flatten()
+    p2 = maps[2].get_position().get_points().flatten()
+
+    ax_cbar = fig.add_axes([p0[0], 0, p1[2]-p0[0], 0.05])
+    plt.colorbar(cto, cax=ax_cbar, orientation='horizontal')
+
+    ax_cbar1 = fig.add_axes([p2[0], 0, p2[2]-p2[0], 0.05])
+    plt.colorbar(ctd, cax=ax_cbar1, orientation='horizontal')
+
+    for m_ax in maps[0:3]:
+        m_ax.contourf(land_mask,
+                      levels=[.5, 1],
+                      colors=[matplotlib.cm.gray(180)],
+                      zorder=3)
+
+    fig.subplots_adjust(hspace=0.2, wspace=0.2)
+
+    animation = FuncAnimation(fig,
+                              update,
+                              range(1, len(fc_da.time)),
+                              interval=100)
+
+    plt.close()
+
+    output_file = os.path.join("plot", ".2020-10-1.{}.png".format(leadtime))
+    logging.info("Saved {}".format(output_file))
+    fig.savefig(output_file)
+    logging.info("Saving plot to {}".format(output_path))
+    animation.save(output_path,
+                   fps=10,
+                   extra_args=['-vcodec', 'libx264'])
+    return animation
 
 
 def sic_error_args() -> object:
