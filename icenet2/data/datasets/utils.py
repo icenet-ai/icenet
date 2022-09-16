38e9061069eb39731d3425c6ef25fd7dc8d32bae
James Byrne <jambyr@bas.ac.uk>
2022-09-16 09:45:56 +0100

    Maxing shuffling out due to high system memory demand

--- icenet2/data/datasets/utils.py
+++ icenet2/data/datasets/utils.py
@@ -129,7 +129,7 @@ class SplittingMixin:
             # FIXME: this is not a good calculation, but we don't have access
             #  in the mixin to the configuration that generated the dataset #57
             train_ds = train_ds.shuffle(
-                int(len(self.train_fns) * self.batch_size))
+                min(int(len(self.train_fns) * self.batch_size), 366))
 
         train_ds = train_ds.\
             map(decoder, num_parallel_calls=self.batch_size).\
