00f7d0c88814bf77674fc9ee3aa083177c87ced4
James Byrne <jambyr@bas.ac.uk>
2023-02-16 14:56:02 +0000

    Ensuring postprocessing of latlon data, though I'm a bit confused about why this wasn't already present

--- icenet/data/interfaces/downloader.py
+++ icenet/data/interfaces/downloader.py
@@ -267,6 +267,8 @@ class ClimateDownloader(Downloader):
             logging.info("No requested dates remain, likely already present")
 
             if os.path.exists(latlon_path):
+                if self._postprocess:
+                    self.postprocess(var, latlon_path)
                 self._files_downloaded.append(latlon_path)
 
     def postprocess(self, var, download_path):
