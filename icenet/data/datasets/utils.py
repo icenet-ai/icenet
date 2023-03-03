57b7527d6ae50b11e45b2b83ce8c5c191304dd60
James Byrne <jambyr@bas.ac.uk>
2023-03-03 23:42:04 +0000

    Dev #124: adding some additional logging for a test run

--- icenet/data/datasets/utils.py
+++ icenet/data/datasets/utils.py
@@ -171,14 +171,26 @@ class SplittingMixin:
                     y = y.numpy()
                     sw = sw.numpy()
 
-                    logging.debug("Got record {} with x {} y {} sw {}".
-                                  format(i,
+                    logging.debug("Got record {}:{} with x {} y {} sw {}".
+                                  format(df,
+                                         i,
                                          x.shape,
                                          y.shape,
                                          sw.shape))
 
                     input_nans = np.isnan(x).sum()
                     output_nans = np.isnan(y[sw > 0.]).sum()
+                    input_min = np.min(x)
+                    input_max = np.max(x)
+                    output_min = np.min(x)
+                    output_max = np.max(x)
+                    sw_min = np.min(x)
+                    sw_max = np.max(x)
+
+                    logging.debug("Bounds: Input {}:{} Output {}:{} SW {}:{}".
+                                  format(input_min, input_max,
+                                         output_min, output_max,
+                                         sw_min, sw_max))
 
                     if input_nans > 0:
                         logging.warning("Input NaNs detected in {}:{}".
