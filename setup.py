3dd501ef8f505e17ab357536305a53fee3fa2797
Bryn N. Ubald <bryald@bas.ac.uk>
2023-11-23 21:43:27 +0000

    Dev #193: Omits setup.py in pre-commit runs

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 from setuptools import setup, find_packages
 
 import icenet
+
 """Setup module for icenet
 """
 
@@ -34,35 +35,44 @@ setup(
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
-    ],
+        ],
     entry_points={
         "console_scripts": [
             "icenet_data_masks = icenet.data.sic.mask:main",
+
             "icenet_data_cmip = icenet.data.interfaces.esgf:main",
             "icenet_data_era5 = icenet.data.interfaces.cds:main",
             "icenet_data_oras5 = icenet.data.interfaces.cmems:main",
             "icenet_data_hres = icenet.data.interfaces.mars:hres_main",
             "icenet_data_seas = icenet.data.interfaces.mars:seas_main",
             "icenet_data_sic = icenet.data.sic.osisaf:main",
+
             "icenet_data_reproc_monthly = "
             "icenet.data.interfaces.utils:reprocess_main",
             "icenet_data_add_time_dim = "
             "icenet.data.interfaces.utils:add_time_dim_main",
+
             "icenet_process_cmip = icenet.data.processors.cmip:main",
             "icenet_process_era5 = icenet.data.processors.era5:main",
             "icenet_process_oras5 = icenet.data.processors.oras5:main",
             "icenet_process_hres = icenet.data.processors.hres:main",
             "icenet_process_sic = icenet.data.processors.osi:main",
+
             "icenet_process_metadata = icenet.data.processors.meta:main",
+
             "icenet_process_condense = "
             "icenet.data.processors.utils:condense_main",
+
             "icenet_dataset_check = icenet.data.dataset:check_dataset",
             "icenet_dataset_create = icenet.data.loader:create",
+
             "icenet_train = icenet.model.train:main",
             "icenet_predict = icenet.model.predict:main",
             "icenet_upload_azure = icenet.process.azure:upload",
             "icenet_upload_local = icenet.process.local:upload",
+
             "icenet_plot_record = icenet.plotting.data:plot_tfrecord",
+
             "icenet_plot_forecast = icenet.plotting.forecast:plot_forecast",
             "icenet_plot_input = icenet.plotting.data:plot_sample_cli",
             "icenet_plot_sic_error = icenet.plotting.forecast:sic_error",
@@ -72,7 +82,9 @@ setup(
             "icenet_plot_sie_error = icenet.plotting.forecast:sie_error",
             "icenet_plot_metrics = icenet.plotting.forecast:metric_plots",
             "icenet_plot_leadtime_avg = icenet.plotting.forecast:leadtime_avg_plots",
+
             "icenet_video_data = icenet.plotting.video:data_cli",
+
             "icenet_output = icenet.process.predict:create_cf_output",
             "icenet_output_geotiff = "
             "icenet.process.forecasts:create_geotiff_output",
@@ -80,17 +92,18 @@ setup(
             "icenet.process.forecasts:broadcast_main",
             "icenet_output_reproject = "
             "icenet.process.forecasts:reproject_main",
+
             "icenet_result_threshold = "
             "icenet.results.threshold:threshold_main"
         ],
-    },
+        },
     python_requires='>=3.7, <4',
     install_requires=get_content("requirements.txt"),
     include_package_data=True,
     extras_require={
         "dev": get_content("requirements_dev.txt"),
         "docs": get_content("docs/requirements.txt"),
-    },
+        },
     test_suite='tests',
     tests_require=['pytest>=3'],
     zip_safe=False,
