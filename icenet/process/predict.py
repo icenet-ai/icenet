6d0a699b44bd70613fd20231f63644d318f81a95
James Byrne <jambyr@bas.ac.uk>
2025-01-15 16:41:57 +0000

    Making it so that geospatial reference data isn't required when it won't work - such as with AMSR

--- icenet/process/predict.py
+++ icenet/process/predict.py
@@ -25,9 +25,8 @@ from download_toolbox.interface import get_dataset_config_implementation
 def get_ref_ds(ds: IceNetDataSet) -> xr.Dataset:
     # TODO: this is a bit nasty, but it works well - STORE the originally downloaded mask files in config
     dl = ds.get_data_loader()
-    ref_file_str = "nh" if dl.north else "sh"
-    masks_path = os.path.dirname(list(dl.config["masks"]["land"]["source_files"].values())[0])
-    ref_file = "{}/ice_conc_{}_ease2-250_cdr-v2p0_200001021200.nc".format(masks_path, ref_file_str)
+    ref_file_str = "north" if dl.north else "south"
+    ref_file = "ref.osisaf.{}.nc".format(ref_file_str)
     return xr.open_dataset(ref_file)
 
 
@@ -93,6 +92,10 @@ def get_args():
 
     ap.add_argument("-m", "--mask", default=False, action="store_true")
 
+    ap.add_argument("-p", "--plain",
+                    default=False,
+                    help="Don't try to add geospatial or complex metadata from ground truth",
+                    action="store_true")
     ap.add_argument("--nan",
                     help="Apply nans, not zeroes, to land mask",
                     default=False,
@@ -117,24 +120,19 @@ def get_args():
 
 
 def create_cf_output():
-    """
-
-    """
     args = get_args()
-
-    dataset_config = \
-        os.path.join(args.root, "dataset_config.{}.json".format(args.dataset))
-    ds = IceNetDataSet(dataset_config)
-    dl = ds.get_data_loader()
-    hemi_str = "north" if dl.north else "south"
-    ref_sic = get_ref_ds(ds)
-    ref_cube = get_ref_cube(ds)
 
     dates = [
         dt.date(*[int(v) for v in s.split("-")])
         for s in args.datefile.read().split()
     ]
     args.datefile.close()
+
+    dataset_config = \
+        os.path.join(args.root, "dataset_config.{}.json".format(args.dataset))
+    ds = IceNetDataSet(dataset_config)
+    dl = ds.get_data_loader()
+    hemi_str = "north" if dl.north else "south"
 
     arr, ens_members = zip(
         *[get_prediction_data(args.root, args.name, date) for date in dates])
@@ -145,67 +143,106 @@ def create_cf_output():
 
     sic_mean = arr[..., 0]
     sic_stddev = arr[..., 1]
-    ground_truth_ds_filename = "data/osisaf/dataset_config.month.hemi.{}.json".format(hemi_str)
-    ground_truth_ds_config = get_dataset_config_implementation(ground_truth_ds_filename)
 
-    if args.mask:
-        # TODO: daily will need to use appropriate reference, so don't leave like this
-        mask_gen = Masks(ground_truth_ds_config)
+    ##
+    # Metadata
+    #
+    data_vars = dict(
+        sic_mean=(["time", "yc", "xc", "leadtime"], sic_mean),
+        sic_stddev=(["time", "yc", "xc", "leadtime"], sic_stddev),
+        ensemble_members=(["time"], ens_members),
+    )
 
-        if args.agcm:
-            logging.info("Applying active grid cell masks")
+    coords = dict(
+        time=[pd.Timestamp(d) for d in dates],
+        leadtime=np.arange(1, arr.shape[3] + 1, 1),
+    )
 
-            for idx, forecast_date in enumerate(dates):
-                for lead_idx in np.arange(0, arr.shape[3], 1):
-                    lead_dt = forecast_date + dt.timedelta(days=int(lead_idx) +
-                                                           1)
-                    logging.debug(
-                        "Active grid cell mask start {} forecast date {}".
-                        format(forecast_date, lead_dt))
+    extra_attrs = dict()
 
-                    grid_cell_mask = mask_gen.active_grid_cell(lead_dt)
-                    sic_mean[idx, grid_cell_mask, lead_idx] = 0
-                    sic_stddev[idx, grid_cell_mask, lead_idx] = 0
+    if not args.plain:
+        ground_truth_ds_filename = "data/osisaf/dataset_config.month.hemi.{}.json".format(hemi_str)
+        ground_truth_ds_config = get_dataset_config_implementation(ground_truth_ds_filename)
 
-        if args.land:
-            logging.info("Land masking the forecast output")
-            land_mask = mask_gen.land()
-            mask = land_mask[np.newaxis, ..., np.newaxis]
-            mask = np.repeat(mask, sic_mean.shape[-1], axis=-1)
-            mask = np.repeat(mask, sic_mean.shape[0], axis=0)
+        lists_of_fcast_dates = [[
+            pd.Timestamp(
+                date + relativedelta({"{}s".format(ground_truth_ds_config.frequency.attribute): int(lead_idx)}))
+            for lead_idx in np.arange(1, arr.shape[3] + 1, 1)
+        ] for date in dates]
 
-            if args.nan:
-                logging.info("Applying nans to land mask")
-                sic_mean[mask] = np.nan
-                sic_stddev[mask] = np.nan
-            else:
-                logging.info("Applying zeros to land mask")
-                sic_mean[mask] = 0
-                sic_stddev[mask] = 0
+        ref_sic = get_ref_ds(ds)
+        ref_cube = get_ref_cube(ds)
 
-    lists_of_fcast_dates = [[
-        pd.Timestamp(date + relativedelta({"{}s".format(ground_truth_ds_config.frequency.attribute): int(lead_idx)}))
-        for lead_idx in np.arange(1, arr.shape[3] + 1, 1)
-    ] for date in dates]
+        # Assigning to parameters for dataarray
+        data_vars['Lambert_Azimuthal_Grid'] = ref_sic.Lambert_Azimuthal_Grid
+        coords['xc'] = ref_cube.coord("projection_x_coordinate").points
+        coords['yc'] = ref_cube.coord("projection_y_coordinate").points
+        coords['lat'] = (("yc", "xc"), ref_cube.coord("latitude").points)
+        coords['lon'] = (("yc", "xc"), ref_cube.coord("longitude").points)
+        coords['forecast_date'] = (("time", "leadtime"), lists_of_fcast_dates)
+
+        # Issue#18: Overcoming OSI-SAF EPSG ref issue
+        extra_attrs = dict(
+            geospatial_lat_min=ref_cube.attributes["geospatial_lat_min"],
+            geospatial_lat_max=ref_cube.attributes["geospatial_lat_max"],
+            geospatial_lon_min=ref_cube.attributes["geospatial_lon_min"],
+            geospatial_lon_max=ref_cube.attributes["geospatial_lon_max"],
+            icenet_ground_truth_ds=ground_truth_ds_filename,
+            icenet_mask_implementation="icenet.data.masks.osisaf:Masks",
+            spatial_resolution=ref_cube.attributes["spatial_resolution"],
+            # Use ISO 8601:2004 duration format, preferably the extended format
+            # as recommended in the Attribute Content Guidance section.
+            time_coverage_start=min(
+                set([item for row in lists_of_fcast_dates
+                     for item in row])).isoformat(),
+            time_coverage_end=max(
+                set([item for row in lists_of_fcast_dates
+                     for item in row])).isoformat(),
+        )
+
+        ##
+        # Masks
+        #
+        if args.mask:
+            # TODO: daily will need to use appropriate reference, so don't leave like this
+            mask_gen = Masks(ground_truth_ds_config)
+
+            if args.agcm:
+                logging.info("Applying active grid cell masks")
+
+                for idx, forecast_date in enumerate(dates):
+                    for lead_idx in np.arange(0, arr.shape[3], 1):
+                        lead_dt = forecast_date + dt.timedelta(days=int(lead_idx) +
+                                                               1)
+                        logging.debug(
+                            "Active grid cell mask start {} forecast date {}".
+                            format(forecast_date, lead_dt))
+
+                        grid_cell_mask = mask_gen.active_grid_cell(lead_dt)
+                        sic_mean[idx, grid_cell_mask, lead_idx] = 0
+                        sic_stddev[idx, grid_cell_mask, lead_idx] = 0
+
+            if args.land:
+                logging.info("Land masking the forecast output")
+                land_mask = mask_gen.land()
+                mask = land_mask[np.newaxis, ..., np.newaxis]
+                mask = np.repeat(mask, sic_mean.shape[-1], axis=-1)
+                mask = np.repeat(mask, sic_mean.shape[0], axis=0)
+
+                if args.nan:
+                    logging.info("Applying nans to land mask")
+                    sic_mean[mask] = np.nan
+                    sic_stddev[mask] = np.nan
+                else:
+                    logging.info("Applying zeros to land mask")
+                    sic_mean[mask] = 0
+                    sic_stddev[mask] = 0
 
     xarr = xr.Dataset(
-        data_vars=dict(
-            Lambert_Azimuthal_Grid=ref_sic.Lambert_Azimuthal_Grid,
-            sic_mean=(["time", "yc", "xc", "leadtime"], sic_mean),
-            sic_stddev=(["time", "yc", "xc", "leadtime"], sic_stddev),
-            ensemble_members=(["time"], ens_members),
-        ),
-        coords=dict(
-            time=[pd.Timestamp(d) for d in dates],
-            leadtime=np.arange(1, arr.shape[3] + 1, 1),
-            forecast_date=(("time", "leadtime"), lists_of_fcast_dates),
-            xc=ref_cube.coord("projection_x_coordinate").points,
-            yc=ref_cube.coord("projection_y_coordinate").points,
-            lat=(("yc", "xc"), ref_cube.coord("latitude").points),
-            lon=(("yc", "xc"), ref_cube.coord("longitude").points),
-        ),
+        data_vars=data_vars,
+        coords=coords,
         # https://wiki.esipfed.org/Attribute_Convention_for_Data_Discovery_1-3
-        attrs=dict(
+        attrs={**dict(
             Conventions="CF-1.6 ACDD-1.3",
             comments="",
             creator_email="jambyr@bas.ac.uk",
@@ -213,18 +250,11 @@ def create_cf_output():
             creator_name="James Byrne",
             creator_url="www.bas.ac.uk",
             date_created=dt.datetime.now().strftime("%Y-%m-%d"),
-            # Issue#18: Overcoming OSI-SAF EPSG ref issue
             geospatial_bounds_crs="EPSG:6931" if dl.north else "EPSG:6932",
-            geospatial_lat_min=ref_cube.attributes["geospatial_lat_min"],
-            geospatial_lat_max=ref_cube.attributes["geospatial_lat_max"],
-            geospatial_lon_min=ref_cube.attributes["geospatial_lon_min"],
-            geospatial_lon_max=ref_cube.attributes["geospatial_lon_max"],
             geospatial_vertical_min=0.0,
             geospatial_vertical_max=0.0,
             hemisphere_string=hemi_str,
             history="{} - creation".format(dt.datetime.now()),
-            icenet_ground_truth_ds=ground_truth_ds_filename,
-            icenet_mask_implementation="icenet.data.masks.osisaf:Masks",
             id="IceNet {}".format(icenet_version),
             institution="British Antarctic Survey",
             keywords=
@@ -250,7 +280,6 @@ def create_cf_output():
             source="""
             IceNet model generation at v{}
             """.format(icenet_version),
-            spatial_resolution=ref_cube.attributes["spatial_resolution"],
             # Values for any standard_name attribute must come from the CF
             # Standard Names vocabulary for the data file or product to
             #  comply with CF
@@ -260,85 +289,81 @@ def create_cf_output():
             IceNet run in an ensemble, with postprocessing to determine
             the mean and standard deviation across the runs.
             """,
-            # Use ISO 8601:2004 duration format, preferably the extended format
-            # as recommended in the Attribute Content Guidance section.
-            time_coverage_start=min(
-                set([item for row in lists_of_fcast_dates
-                     for item in row])).isoformat(),
-            time_coverage_end=max(
-                set([item for row in lists_of_fcast_dates
-                     for item in row])).isoformat(),
             time_coverage_duration="P1D",
             time_coverage_resolution="P1D",
             title="Sea Ice Concentration Prediction",
-        ))
+        ), **extra_attrs})
 
-    xarr.time.attrs = dict(
-        long_name=ref_cube.coord("time").long_name,
-        standard_name=ref_cube.coord("time").standard_name,
-        axis="T",
-        # TODO: https://github.com/SciTools/cf-units for units methods
-        # units=Unit('seconds since 1978-01-01 00:00:00', calendar='gregorian')
-        # bounds=array([[31622400., 31708800.]])
-    )
-    xarr.yc.attrs = dict(
-        long_name=ref_cube.coord("projection_y_coordinate").long_name,
-        standard_name=ref_cube.coord("projection_y_coordinate").standard_name,
-        units=ref_cube.coord("projection_y_coordinate").units.name,
-        axis="Y",
-        # TODO: iris.coord_systems.LambertAzimuthalEqualArea
-    )
-    xarr.xc.attrs = dict(
-        long_name=ref_cube.coord("projection_x_coordinate").long_name,
-        standard_name=ref_cube.coord("projection_x_coordinate").standard_name,
-        units=ref_cube.coord("projection_x_coordinate").units.name,
-        axis="X",
-    )
-    xarr.leadtime.attrs = dict(
-        long_name="leadtime of forecast in relation to reference time",
-        short_name="leadtime",
-        # TODO: days, months etc from ground_truth_ds_config.frequency.attribute
-        # units="1",
-    )
+    ##
+    # Variable attributes
+    #
+    if not args.plain:
+        xarr.time.attrs = dict(
+            long_name=ref_cube.coord("time").long_name,
+            standard_name=ref_cube.coord("time").standard_name,
+            axis="T",
+            # TODO: https://github.com/SciTools/cf-units for units methods
+            # units=Unit('seconds since 1978-01-01 00:00:00', calendar='gregorian')
+            # bounds=array([[31622400., 31708800.]])
+        )
+        xarr.yc.attrs = dict(
+            long_name=ref_cube.coord("projection_y_coordinate").long_name,
+            standard_name=ref_cube.coord("projection_y_coordinate").standard_name,
+            units=ref_cube.coord("projection_y_coordinate").units.name,
+            axis="Y",
+            # TODO: iris.coord_systems.LambertAzimuthalEqualArea
+        )
+        xarr.xc.attrs = dict(
+            long_name=ref_cube.coord("projection_x_coordinate").long_name,
+            standard_name=ref_cube.coord("projection_x_coordinate").standard_name,
+            units=ref_cube.coord("projection_x_coordinate").units.name,
+            axis="X",
+        )
+        xarr.leadtime.attrs = dict(
+            long_name="leadtime of forecast in relation to reference time",
+            short_name="leadtime",
+            # TODO: days, months etc from ground_truth_ds_config.frequency.attribute
+            # units="1",
+        )
 
-    xarr.lat.attrs = dict(
-        long_name=ref_cube.coord("latitude").long_name,
-        standard_name=ref_cube.coord("latitude").standard_name,
-        units=ref_cube.coord("latitude").units.name,
-    )
-    xarr.lon.attrs = dict(
-        long_name=ref_cube.coord("longitude").long_name,
-        standard_name=ref_cube.coord("longitude").standard_name,
-        units=ref_cube.coord("longitude").units.name,
-    )
+        xarr.lat.attrs = dict(
+            long_name=ref_cube.coord("latitude").long_name,
+            standard_name=ref_cube.coord("latitude").standard_name,
+            units=ref_cube.coord("latitude").units.name,
+        )
+        xarr.lon.attrs = dict(
+            long_name=ref_cube.coord("longitude").long_name,
+            standard_name=ref_cube.coord("longitude").standard_name,
+            units=ref_cube.coord("longitude").units.name,
+        )
 
-    xarr.sic_mean.attrs = dict(
-        long_name="mean sea ice area fraction across ensemble runs of icenet "
-        "model",
-        standard_name="sea_ice_area_fraction",
-        short_name="sic",
-        valid_min=0,
-        valid_max=1,
-        ancillary_variables="sic_stddev",
-        grid_mapping="Lambert_Azimuthal_Grid",
-        units="1",
-    )
+        xarr.sic_mean.attrs = dict(
+            long_name="mean sea ice area fraction across ensemble runs of icenet "
+            "model",
+            standard_name="sea_ice_area_fraction",
+            short_name="sic",
+            valid_min=0,
+            valid_max=1,
+            ancillary_variables="sic_stddev",
+            grid_mapping="Lambert_Azimuthal_Grid",
+            units="1",
+        )
 
-    xarr.sic_stddev.attrs = dict(
-        long_name=
-        "total uncertainty (one standard deviation) of concentration of sea ice",
-        standard_name="sea_ice_area_fraction standard_error",
-        valid_min=0,
-        valid_max=1,
-        grid_mapping="Lambert_Azimuthal_Grid",
-        units="1",
-    )
+        xarr.sic_stddev.attrs = dict(
+            long_name=
+            "total uncertainty (one standard deviation) of concentration of sea ice",
+            standard_name="sea_ice_area_fraction standard_error",
+            valid_min=0,
+            valid_max=1,
+            grid_mapping="Lambert_Azimuthal_Grid",
+            units="1",
+        )
 
-    xarr.ensemble_members.attrs = dict(
-        long_name="number of ensemble members used to create this prediction",
-        short_name="ensemble_members",
-        # units="1",
-    )
+        xarr.ensemble_members.attrs = dict(
+            long_name="number of ensemble members used to create this prediction",
+            short_name="ensemble_members",
+            # units="1",
+        )
 
     # TODO: split into daily files
     output_path = os.path.join(args.output_dir, "{}.nc".format(args.name))
