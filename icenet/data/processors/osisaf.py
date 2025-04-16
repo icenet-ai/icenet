6b1f45d9ecc58a11bb41bfb4373534748932aa2e
James Byrne <jambyr@bas.ac.uk>
2025-04-16 16:51:25 +0100

    Handling regridding processing for preprocess_regrid

--- icenet/data/processors/osisaf.py
+++ icenet/data/processors/osisaf.py
@@ -18,10 +18,14 @@ class SICPreProcessor(NormalisingChannelProcessor):
             raise RuntimeError("OSISAF SIC implementation should be dealing "
                                "with siconca only")
         else:
-            da.xc.attrs["units"] = "metres"
-            da.yc.attrs["units"] = "metres"
-            da['xc'] = da.xc * 1000
-            da['yc'] = da.yc * 1000
+            # If the OSISAF data has been regridded, we might end up with renamed coords
+            da = da.rename(dict(x="xc", y="yc"))
+
+            if "units" in da.xc.attrs and da.xc.attrs["units"] == "km":
+                da.xc.attrs["units"] = "metres"
+                da.yc.attrs["units"] = "metres"
+                da['xc'] = da.xc * 1000
+                da['yc'] = da.yc * 1000
 
         #     masks = Masks(north=self.north, south=self.south)
         #     return sic_interpolate(da, masks)
@@ -31,13 +35,27 @@ class SICPreProcessor(NormalisingChannelProcessor):
 def amsr_coordinate_regrid(ref_cube, cube, *args):
     (sic_ref, ) = args
     logging.info(f"Using custom regrid, loading {sic_ref}")
+    osi_sic = xr.open_dataset(sic_ref).ice_conc
 
-    da = xr.open_dataset(sic_ref).ice_conc
-    y_coord = iris.coords.DimCoord(da.yc * 1000, standard_name='projection_y_coordinate', units='m')
-    x_coord = iris.coords.DimCoord(da.xc * 1000, standard_name='projection_x_coordinate', units='m')
+    # NOTE: this is normally required for raw data, but not if the coordinates have been stripped
+    # cube.remove_coord('projection_y_coordinate')
+    # cube.remove_coord('projection_x_coordinate')
+
+    # Create coordinate objects
+    y_coord = iris.coords.DimCoord(osi_sic.yc * 1000, standard_name='projection_y_coordinate', units='m')
+    x_coord = iris.coords.DimCoord(osi_sic.xc * 1000, standard_name='projection_x_coordinate', units='m')
     cube.add_dim_coord(y_coord, 1)
     cube.add_dim_coord(x_coord, 2)
 
-    cube.coord('projection_y_coordinate').coord_system = ref_cube.coord('projection_y_coordinate')[0].coord_system
-    cube.coord('projection_x_coordinate').coord_system = ref_cube.coord('projection_x_coordinate')[0].coord_system
+    # Ref: Version 3.1 April 2023 of the OSI-450 product user manual
+    cs = iris.coord_systems.PolarStereographic(central_lat=90.0 if osi_sic.lat.min() > 0 else -90,
+                                               central_lon=0,
+                                               false_easting=0.0,
+                                               false_northing=0.0,
+                                               # true_scale_lat=70.0,
+                                               ellipsoid=iris.coord_systems.GeogCS(semi_major_axis=6378137.0,
+                                                                                   semi_minor_axis=6356752.314245))
+    cube.coord('projection_y_coordinate').coord_system = cs
+    cube.coord('projection_x_coordinate').coord_system = cs
+
     return cube
