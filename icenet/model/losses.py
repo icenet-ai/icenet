1bb373385ba5cd44e0ab066355dd136a358136a1
James Byrne <jambyr@bas.ac.uk>
2025-06-06 12:00:50 +0100

    Loss update

--- icenet/model/losses.py
+++ icenet/model/losses.py
@@ -22,10 +22,12 @@ class WeightedMSE(tf.keras.losses.MeanSquaredError):
         """
 
         # TF automatically reduces along final dimension - include dummy axis
-        y_true = tf.expand_dims(y_true, axis=-1)
-        y_pred = tf.expand_dims(y_pred, axis=-1)
+        if y_true.shape[-1] != 1:
+            y_true = tf.expand_dims(y_true, axis=-1)
+        if y_pred.shape[-1] != 1:
+            y_pred = tf.expand_dims(y_pred, axis=-1)
 
-        if sample_weight is not None:
+        if sample_weight is not None and sample_weight.shape[-1] != 1:
             sample_weight = tf.expand_dims(sample_weight, axis=-1)
 
         return super().__call__(100 * y_true,
